1562123c65533647719868d78c84bddb5561e433
Abhishek Diwan <abhishekdiwan1227@gmail.com>
2026-07-16 14:07:14 +0530

    Delete branches with no open pull requests
    Remove branches without open pull requests and delete them from remote.

--- stale-branches/fetch-prs.sh
+++ stale-branches/fetch-prs.sh
@@ -1,15 +1,14 @@
 #! /usr/bin/bash
 
 readarray -t branches < <(git fetch --all | git branch --remote | grep -vE 'master|main')
-branches_to_delete=()
 echo "# deleted branches"
 for branch in ${branches[@]}; do
 	branch_name=$(echo $branch | sed 's/origin\///')
 	echo "branch: $branch_name"
 	open_pr=$(gh pr list --head $branch_name --json number | jq length)
 	if [ $open_pr -eq 0 ]; then
-		branches_to_delete+=$branch_name
-		echo "$branch_name added to delete list"
+		echo "deleting $branch_name"
+		git push origin --delete $branch_name
 		echo "- $branch_name" >> $GITHUB_STEP_SUMMARY
 	else
 		echo "no open pull requests for $branch_name"
