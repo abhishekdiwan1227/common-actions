11c7241fde01f8570aaa5f6662ac4b008ae02495
Abhishek Diwan <abhishekdiwan1227@gmail.com>
2026-07-16 11:55:48 +0530

    Fix syntax error in fetch-prs.sh

--- stale-branches/fetch-prs.sh
+++ stale-branches/fetch-prs.sh
@@ -6,7 +6,7 @@ for branch in $branches; do
 	branch_name=$(echo $branch | sed 's/origin\///')
 	echo "branch: $branch_name"
 	open_pr=$(gh pr list --head $branch_name --json number | jq length)
-	if [ $open_pr -eq 0]; then
+	if [ $open_pr -eq 0 ]; then
 		branches_to_delete+=$branch_name
 		echo "$branch_name added to delete list"
 	else
