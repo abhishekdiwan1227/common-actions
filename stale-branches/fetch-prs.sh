995705666db91fe08724611f337436290b142af5
Abhishek Diwan <abhishekdiwan1227@gmail.com>
2026-07-16 11:50:48 +0530

    Update fetch-prs.sh

--- stale-branches/fetch-prs.sh
+++ stale-branches/fetch-prs.sh
@@ -7,10 +7,10 @@ for branch in $branches; do
 	echo "branch: $branch_name"
 	open_pr=$(gh pr list --head $branch_name --json number | jq length)
 	if [ $open_pr -eq 0]; then
-		echo "no open pull requests for $branch_name"
-	else
 		branches_to_delete+=$branch_name
 		echo "$branch_name added to delete list"
+	else
+		echo "no open pull requests for $branch_name"
 	fi
 done
 echo branches=${branches_to_delete[@]} >> "$GITHUB_OUTPUT"
