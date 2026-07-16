64557722daba4fd99e38576ffc8aecf88426b65c
Abhishek Diwan <abhishekdiwan1227@gmail.com>
2026-07-16 11:01:43 +0530

    Change open PR count to use jq for length

--- stale-branches/fetch-prs.sh
+++ stale-branches/fetch-prs.sh
@@ -4,6 +4,6 @@ readarray -t branches < <(git branch --remote | grep -vE 'master|main')
 for branch in $branches; do
 	branch_name=$(echo $branch | sed 's/origin\///')
 	echo "branch: $branch_name"
-	open_pr=$(gh pr list --head $branch_name --json number)
+	open_pr=$(gh pr list --head $branch_name --json number | jq length)
 	echo "open_pr=$open_pr" >>$GITHUB_OUTPUT
 done
